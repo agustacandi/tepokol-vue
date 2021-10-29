13f7de6577f83a15ddc8ef4b8e22c1bf31a736d1
Alif Triadi Agung W <aliftriadi.gaming@gmail.com>
2021-10-29 22:44:21 +0700

    Add About

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="md:w-10/12 lg:w-full p-10 w-72 text-center rounded-xl shadow-xl bg-white relative z-30">
+        <img :src="'../assets/images/orang.png'" alt="Orang" class="mx-auto" />
+        <h3 class="pt-5 font-semibold text-yellow-400">{{ name }}</h3>
+        <p class="pb-5 text-gray-600">{{ jabatan }}</p>
+        <div class="flex md:justify-center md:space-x-10 justify-around">
+            <a @click="redirect(facebook)"><img src="../assets/images/facebook.svg" alt="Facebook Icon" /></a>
+            <a @click="redirect(twitter)"><img src="../assets/images/twitter.svg" alt="Twitter Icon" /></a>
+            <a @click="redirect(linked)"><img src="../assets/images/linkedin.svg" alt="Linkedin Icon" /></a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        name: String,
+        image: String,    
+        jabatan: String,    
+        facebook: String,    
+        twitter: String,    
+        linked: String,    
+    },
+    methods: {
+        redirect: function(link = '/', target = '_blank') {
+            window.open(link, target)
+        }
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Gallery from '../views/Gallery.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/event',
-    name: 'Event',
-    component: About
-  },
-  {
-    path: '/gallery',
-    name: 'Gallery',
-    component: Gallery
-  },
-  {
-    path: '/blog',
-    name: 'Blog',
-    component: About
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: About
-  },
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/event',
+        name: 'Event',
+        component: Event
+    },
+    {
+        path: '/gallery',
+        name: 'Gallery',
+        component: Gallery
+    },
+    {
+        path: '/blog',
+        name: 'Blog',
+        component: About
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: About
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
